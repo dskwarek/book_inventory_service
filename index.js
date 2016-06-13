7c8e27fc37b1eb6dc1facb1c998f276500ecb929
dskwarek <dskwarek@motorolasolutions.com>
2016-06-13 11:00:22 +0200

    express added

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 //   Copyright (C) 2016 Motorola Solutions, Inc.
 // </copyright>
 
-var http = require('http');
-var server = http.createServer(function (reg,res) {
-    res.end('hello world');
-});
+var express = require('express');
+var app = express();
+
+app.get('/', function (req,res) {
+    res.send('Hello World');
+})
+
+app.listen(3000, function () {
+    console.log(' App listening on port 3000');
+})
