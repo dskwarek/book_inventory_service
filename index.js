98e6682b2ea84f58c1d6c520f2a345ab5f5151bb
dskwarek <dskwarek@motorolasolutions.com>
2016-06-13 11:09:37 +0200

    middleware added

--- index.js
+++ index.js
@@ -5,10 +5,26 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req,res) {
+app.use(function (req, res, next) {
+    console.log('new request at ' + new Date());
+    next();
+});
+
+app.use(function (req, res, next) {
+    console.log('auth ' + new Date());
+    next();
+});
+
+var f = function(req,res,next){
+    console.log('local ' + new Date());
+    next();
+};
+
+app.get('/',f,
+    function (req,res) {
     res.send('Hello World');
-})
+});
 
 app.listen(3000, function () {
     console.log(' App listening on port 3000');
-})
+});
