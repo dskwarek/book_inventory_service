ed3fa48b41bd52b183997e67b5094a4b586aa872
dskwarek <dskwarek@motorolasolutions.com>
2016-06-14 10:39:19 +0200

    infrastructure configuration

--- infrastructure/heroku.js
+++ infrastructure/heroku.js
@@ -5,3 +5,21 @@ var configurator = heroin(process.env.HEROKU_API_TOKEN);
 configurator.export('bok-inventory').then(function (result) {
     console.log(result);
 })
+
+var prod = { name: 'bok-inventory',
+    region: 'eu',
+    stack: 'cedar-14',
+    //config_vars: { MONGODB_URI: process.env.MONGODB_URI },
+    addons: { mongolab: { plan: 'mongolab:sandbox' } },
+    collaborators: [ 'dskwarek@gmail.com', 'mart.tm@gmail.com' ],
+    features:
+    { 'runtime-dyno-metadata': { enabled: false },
+        'log-runtime-metrics': { enabled: false },
+        'http-session-affinity': { enabled: false },
+        preboot: { enabled: false },
+        'http-shard-header': { enabled: false },
+        'http-end-to-end-continue': { enabled: false },
+        'http-sni': { enabled: false } },
+    formation: [ { process: 'web', quantity: 1, size: 'Free' } ],
+    log_drains: []
+};
