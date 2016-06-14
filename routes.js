b7bcea10a299625188f6ab46e0acb9ea596a37f2
dskwarek <dskwarek@motorolasolutions.com>
2016-06-14 16:08:17 +0200

    export infrastructure added

--- routes.js
+++ routes.js
@@ -4,7 +4,13 @@ module.exports = function(repository) {
     return {
         findAll: function (req, res, next) {
             repository.findAll().then(function (result) {
-                res.json(result);
+                res.format({
+                    'text/html': function(){
+                        res.send('<h1>Hello</h1>');
+                    },
+                    'application/json': function(){
+                        res.json(result);
+                    },});
             }).catch(next);
         }
         ,
